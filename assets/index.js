041be704bc8c9be4df414ad2e6608d866258899f
Daniel Bonn <danieltbonn@gmail.com>
2023-08-07 21:13:47 -0500

    added better messages for message prompts

--- assets/index.js
+++ assets/index.js
@@ -104,17 +104,31 @@ readmeQuestions = [
     {
         type: "input",
         name: "description",
-        message: "Enter a description of the project.",
+        // message: "Enter a description of the project.",
+        message: `Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+        - What was your motivation?
+        - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+        - What problem does it solve?
+        - What did you learn?\n`,
     },
     {
         type: "input",
         name: "installationInstructions",
-        message: "Enter instructions for installing the project.",
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
     },
     {
         type: "input",
         name: "usageInformation",
-        message: "Enter usage information for the project.",
+        // message: "Enter usage information for the project.",
+        message : `Provide instructions and examples for use. Include screenshots as needed.
+
+    To add a screenshot, create an \`assets/images\` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    \`\`\`    md
+    ![alt text](assets/images/screenshot.png)
+    \`\`\`
+    `,
     },
     {
         type: "input",
